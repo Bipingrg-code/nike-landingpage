2ab8a7225880c87e60a103398a3741fc92d16d72
Bipin Gurung <bipin67678grg@gmail.com>
2023-08-28 18:47:45 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+      <section className="padding">PopulerProducts</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">SpecialOffers</section>
+      <section className="bg-pale-blue padding ">Customer Reviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Suscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
+  );
+};
+export default App;

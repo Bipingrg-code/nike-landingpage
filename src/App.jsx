6a2343261e5cc7ab68598cee4d97be1812f4a6f2
Bipin Gurung <bipin67678grg@gmail.com>
2023-08-28 23:12:57 +0545

    navbar and create layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import {
+  Hero,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  SuperQuality,
+  CustomerReviews,
+  Subscribe,
+} from "./sections/index";
+import Nav from './components/Nav'
+
 const App = () => {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-      <section className="padding">PopulerProducts</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">SpecialOffers</section>
-      <section className="bg-pale-blue padding ">Customer Reviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Suscribe</section>
-      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+     <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="bg-pale-blue padding ">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
     </main>
   );
 };

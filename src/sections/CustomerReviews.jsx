0ba0c722975216250a9a69a41ccb604dbaa8e728
Bipin Gurung <bipin67678grg@gmail.com>
2023-09-04 23:31:33 +0545

    add customer reviwes and suscribes section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,30 @@
+import ReviewsCard from "../components/ReviewsCard";
+import { reviews } from "../constants";
+
 const CustomerReviews = () => {
-  return <section>CustomerReviews</section>;
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our <span className="text-coral-red"> Customers </span> Says
+      </h3>
+
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our setisfied customers abouth their
+        expectional experince with us
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewsCard
+            key={review.customerName}
+            customerName={review.customerName}
+            imgURL={review.imgURL}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReviews;
